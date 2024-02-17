daf01395b40e5f4033f64dd1045091f143cd4fec
Jesuloluwa Solanke <jesuloluwap@gmail.com>
2024-02-17 22:51:39 +0100

    added fuctionalities to the cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -2,12 +2,24 @@ import Link from "next/link";
 import { useShoppingCart } from "use-shopping-cart";
 
 function cart() {
-  const { cartCount } = useShoppingCart();
+  const { cartCount, formattedTotalPrice, clearCart } = useShoppingCart();
 
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       {cartCount > 0 ? (
-        <div />
+        <>
+          <h2 className="text-4xl font-semibold">Your shopping cart.</h2>
+          <p className="mt-1 text-xl">
+            {cartCount}
+            items{" "}
+            <button
+              className="opacity-50 hover:opacity-100 text-base capitalize"
+              onClick={() => clearCart()}
+            >
+              (clear all)
+            </button>
+          </p>
+        </>
       ) : (
         <>
           <h2 className="text-4xl font-semibold">
@@ -21,6 +33,20 @@ function cart() {
             </Link>
           </p>
         </>
+      )}
+
+      {cartCount > 0 && (
+        <div className="mt-12 space-y-4">
+          <div className="flex flex-col items-end border-t py- mt-8">
+            <p className="text-xl">
+              Total:{" "}
+              <span className="font-semibold">{formattedTotalPrice}</span>
+            </p>
+            <button className="border rounded py-2 px-6 bg-yellow-500 hover:bg-yellow-600 border-yellow-500 hover:border-yellow-600 focus-ring-4 focus:ring-opacity-50 focus:ring-yellow-500 text-white transition-colors disabled:opacity-50 disabled:cursor-not-allowed  disabled:hover:bg-yellow-500 mt-4 max-w-max">
+              Go to Checkout
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
