8fe31b15c37b90fb7b93804f0dfb4f6999c84631
Jésùloluwa <jesuloluwap@gmail.com>
2024-02-19 22:01:03 +0100

    Update CartProduct.js

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,7 +1,43 @@
+import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function CartProduct({ product }) {
-  return <div></div>;
+  return (
+    <div className="flex justify-between space-x-4 hover:shadow-lg hover:border-opacity-50 border border-opacity-0 rounded-md p-4 bg-white">
+      <Link
+        href={`/product/${product.id}`}
+        className="flex items-center space-x-4 group"
+      >
+        <div className="relative w-20 h-20 group-hover:scale-110 transition-transform">
+          <Image
+            src={product.image}
+            alt={product.name}
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <p className="font-semibold text-xl group-hover:underline">
+          {product.name}
+        </p>
+      </Link>
+      <div className="flex items-center">
+        <div className="flex items-center space-x-">
+          <button
+            disabled={product.quantity <= 1}
+            className="p-1 rounded-md hover:bg-rose-100 hover:text-rose-500"
+          >
+            <MinusSmallIcon className="w-6 h-6 flex-shrink-0" />
+          </button>
+          <p className="font-semibold text-xl">{product.quantity}</p>
+          <button className="p-1 rounded-md hover:bg-green-100 hover:text-green-500">
+            <PlusSmallIcon className="w-6 h-6 flex-shrink-0" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartProduct;
