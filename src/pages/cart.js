3765d8dff058fdb374f1e71fdfbcab892b05d221
Jesuloluwa Solanke <jesuloluwap@gmail.com>
2024-02-21 19:27:02 +0100

    .

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import { useShoppingCart } from "use-shopping-cart";
+import CartProduct from "src/components/CartProduct";
+import { useState } from "react";
+import axios from "axios";
 
 function Cart() {
   const {
